efe38aeaff39dc12d3e2942e2abeab970cd31362
Alayande-442 <alayandesteven@gmail.com>
2025-02-07 17:00:36 +0100

    FIX: form validation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+// COMMENT Navbar component.
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
